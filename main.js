b2e777764326b30274ccf6712eeb00525b280628
Farbice <109209812+Farbice@users.noreply.github.com>
2023-07-29 23:59:17 +0200

    Further examples and tests

--- main.js
+++ main.js
@@ -17,3 +17,26 @@ const retour = bingo(arrf, value => value + 1);
 console.log(retour);
 console.log(arrf.map((value) => {return value * 53}));
 console.log(arrf.map((value) => {return 1/value}));
+
+
+
+const moodindex = [1, 2, 3, 4];
+
+function moodSelect(tab, callback){
+    
+    const moodResult = [];
+
+    for(i = 0; i < tab.length; i++){
+        moodResult.push(callback(tab[i]));
+    }
+
+    return moodResult;
+
+}
+
+
+const moodOutput = moodSelect(moodindex, value => value / (value + 0.85));
+
+console.log(moodOutput);
+console.log(arrf.map((value) => {return value / (value + 0.85)}));
+
